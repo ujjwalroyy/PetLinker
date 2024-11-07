1ac0201a9c5865d83f9452de83053bd8ccbd1423
Ujjwal Kumar Roy <139360582+ujjwalroyy@users.noreply.github.com>
2024-11-07 23:36:08 +0530

    Update AdminController.js

--- server/Controller/AdminController.js
+++ server/Controller/AdminController.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const getCredentials = (req, res) => {
     const Credentials = { "username": "Mr. Developer", "password": "safer" }
-    res.status(200).json(Credentials)
+    res.status(200).json(Credentials)  
 }
 
 module.exports = { getCredentials }
